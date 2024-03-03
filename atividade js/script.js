6c802ffe4518e6526025fea10de574ce496a09dd
Matheus Marques <matheusmarquesportela@gmail.com>
2024-03-03 13:19:32 -0300

    update

--- atividade js/script.js
+++ atividade js/script.js
@@ -1,7 +1,9 @@
 document.getElementById('form').addEventListener('submit', function(event) {
     event.preventDefault();
 
+    var matricula = parseInt(document.getElementById('matricula').value)
     var nome = document.getElementById('name').value
+    var materia = document.getElementById('materia').value
     var nota1 = parseInt(document.getElementById('nota1').value)
     var nota2 = parseInt(document.getElementById('nota2').value)
     var nota3 = parseInt(document.getElementById('nota3').value)
@@ -11,8 +13,9 @@ document.getElementById('form').addEventListener('submit', function(event) {
     var resultado = document.getElementById('resultado')
     var mensagem = document.getElementById('mensagem')
 
-    if (media >= 70) {
-        mensagem.innerText = `Parabéns ${nome}, você está aprovado! Sua média é ${media}.`;
+    if (media >= 60) {
+        mensagem.innerText = `Matricula: ${matricula} 
+        ${nome}, você está aprovado! Sua média é ${media}.`;
     } else {
         mensagem.innerText = `Infelizmente ${nome} você está reprovado! Sua média foi de ${media}.`;
     }
