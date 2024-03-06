cb4cb2a295e586aee57f2dafb493e76c9a15cdce
Matheus Marques <matheusmarquesportela@gmail.com>
2024-03-06 13:01:04 -0300

    update

--- atividade js/script.js
+++ atividade js/script.js
@@ -3,7 +3,8 @@ document.getElementById('form').addEventListener('submit', function(event) {
 
     var matricula = parseInt(document.getElementById('matricula').value)
     var nome = document.getElementById('name').value
-    var materia = document.getElementById('materia').value
+    var opcaoMateria = document.getElementById('imateria');
+    var materia = opcaoMateria.options[opcaoMateria.selectedIndex].text;
     var nota1 = parseInt(document.getElementById('nota1').value)
     var nota2 = parseInt(document.getElementById('nota2').value)
     var nota3 = parseInt(document.getElementById('nota3').value)
@@ -14,14 +15,28 @@ document.getElementById('form').addEventListener('submit', function(event) {
     var mensagem = document.getElementById('mensagem')
 
     if (media >= 60) {
-        mensagem.innerText = `Matricula: ${matricula} 
-        ${nome}, você está aprovado! Sua média é ${media}.`;
+        var status = 'Aprovado'
+    } else{
+        var status = 'Reprovado'
+    }
+
+    mensagem.innerText = 
+        `Matricula: ${matricula} 
+        Nome completo: ${nome}
+        Materia: ${materia}
+        Média: ${media}
+        Status: ${status}`;
+
+    /*if (media >= 60) {
+        mensagem.innerText = 
+        `Matricula: ${matricula} 
+        Nome completo: ${nome}
+        Materia: 
+        Média: ${media}
+        Status: ${status}`;
     } else {
         mensagem.innerText = `Infelizmente ${nome} você está reprovado! Sua média foi de ${media}.`;
-    }
+    }*/
 
     resultado.style.display = 'block';
 })
-
-console.log(nome)
-console.log(media)
